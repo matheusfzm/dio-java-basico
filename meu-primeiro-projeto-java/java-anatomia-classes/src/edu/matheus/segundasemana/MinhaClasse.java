9449a6c9c77903c0ae1944c619f8dab10bd530f9
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-06 10:39:31 -0300

    anatomia de classe

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/edu/matheus/segundasemana/MinhaClasse.java
@@ -0,0 +1,20 @@
+package edu.matheus.segundasemana;
+public class MinhaClasse {
+
+public static void main(String[] args) {
+    String primeiroNome = "Matheus";
+    String segundoNome = "Macedo";
+    
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    
+    System.out.println(nomeCompleto);
+}    
+
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
+
+}
